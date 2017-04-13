84073a79702f7c7d9fee61dbe63e95742a9c6335
Artur Trofimchuk <artspace2483@gmail.com>
2017-04-13 17:00:14 +0300

    Update LoginHelper.java

--- src/viseven/LoginHelper.java
+++ src/viseven/LoginHelper.java
@@ -4,13 +4,13 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginHelper {
-	public static String baseUrl = "http://viseven.feature.com.ua/";
+	public static String baseUrl = "http://xxxxx.feature.com.ua/";
 	
 	public static void login(WebDriver driver) {
 		driver.findElement(By.id("UserName")).clear();
-		driver.findElement(By.id("UserName")).sendKeys("art@qapint.com");
+		driver.findElement(By.id("UserName")).sendKeys("xxxxxx@qapint.com");
 		driver.findElement(By.id("password")).clear();
-		driver.findElement(By.id("password")).sendKeys("pass4test");
+		driver.findElement(By.id("password")).sendKeys("xxxxxx");
 		driver.findElement(By.id("btnLogin")).click();
 	}
 }
